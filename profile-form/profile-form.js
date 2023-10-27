8a8289671afa0bd9de18c79f178073c96b919fea
Maera <baeitsmae@gmail.com>
2023-10-27 20:51:54 +0200

    astro section

--- profile-form/profile-form.js
+++ profile-form/profile-form.js
@@ -2,6 +2,8 @@ const nextBtn = document.querySelector(".next-header")
 const header = document.querySelector(".profile-form-page header")
 const headerBtn = document.querySelector(".profile-form-page .btn")
 const sectionRelationType = document.querySelector(".relation-type")
+const sectionAstro = document.querySelector(".astro")
+const nextRelationType = document.querySelector(".next-relation-type")
 
 nextBtn.addEventListener("click", () => {
     header.style.display = "none"
@@ -9,10 +11,14 @@ nextBtn.addEventListener("click", () => {
     sectionRelationType.style.display = "flex"
 })
 
+nextRelationType.addEventListener("click", (e) => {
+    e.preventDefault()
+    sectionRelationType.style.display = "none"
+    sectionAstro.style.display = "block"
+})
+
 const checkInput = document.querySelectorAll(".check-input")
 const checkbox = document.querySelectorAll(".checkbox")
-
-
 
 checkInput.forEach(input => {
     input.addEventListener("click", () => {
@@ -23,5 +29,4 @@ checkInput.forEach(input => {
             input.parentNode.style.background = "initial"
         }
     })
-    console.log(input.parentNode)
 })
