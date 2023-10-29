62d064468a8a31c8b98cecdb46f69e92aa8177f8
Maera <baeitsmae@gmail.com>
2023-10-29 11:29:27 +0100

    dashboard swipes

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -6,3 +6,40 @@ noMatchBtn.addEventListener("click", () => {
     sectionSwipes.style.display = "none"
     sectionSwipes2.style.display = "block"
 })
+
+const clickItem = document.querySelectorAll(".clic")
+const dashboard = document.querySelector(".dashboard")
+
+clickItem.forEach(item => {
+    item.addEventListener("click", () => {
+        const msgClic = document.createElement("form")
+        const msg = document.createElement("textarea")
+        const submitBtn = document.createElement("button")
+        const closeMsg = document.createElement("img")
+
+        console.log(item)
+
+        msgClic.classList.add("msg-clic")
+        msg.classList.add("msg")
+        submitBtn.classList.add("msg-submit")
+
+        closeMsg.setAttribute("src", "../assets/no-match.png")
+        msg.setAttribute("placeholder", "Engage la conversation en décrivant ce qui te vient en tête avec cet élément.")
+
+        submitBtn.textContent = "Envoyer"
+
+        dashboard.appendChild(msgClic)
+        msgClic.appendChild(msg)
+        msgClic.appendChild(submitBtn)
+        msgClic.appendChild(closeMsg)  
+
+        closeMsg.addEventListener("click", () => {
+            msgClic.style.display = "none"
+        })
+
+        submitBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            msgClic.style.display = "none"
+        })
+    })
+})
